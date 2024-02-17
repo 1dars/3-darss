5af447c4583b122e97507d5661e05351246b135a
1dars <jumadullaeviskandar@gmail.com>
2024-02-17 13:01:05 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# 1 misol
+# b = [14, 23, 65, "a", "b", "c", "gf", 65]
+# f = []
+# for a in b:
+#     if type(a) == int:
+#         f.append(a)
+# print(f)
+from typing import List
+
+# 3 misol
+# a = [3,5,12,7,0,67,9,45,]
+# b = []
+# for x in a:
+#     if 6 < x:
+#         b.append(x)
+# print(b)
+
+# 4 misol
+# a = int(input("hohlagan son kiriting "))
+#
+# son = [1,2,3,4,5,6,7,8,9]
+# son.remove(1)
+# son.append(a)
+# print(son)
+
+
+# # 6 misol
+# lst = [1,2,3,4,5,67,]
+# a = []
+# for x in lst:
+#     lst.copy()
+#     a.append(lst)
+# print(a)
+
+# 10 misol
+# son = [9, 2, "space", "car", "lion", 16]
+# a = []
+# for x in son:
+#     if type(x) == int:
+#         a.append(x)
+# print(a)
+
+# 8 misol
+# sonlar = [1,2,3,4,5,6,7,8,9]
+# num = int(input("Son: "))
+# sonlar.pop(0)
+# sonlar.append(num)
+# print(sonlar)
+
+
